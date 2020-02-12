8983db9966ad740ff1fbe50846db1c1dfe5b510e
Suphakit-Somwanich <suphakit_somwanich@cmu.ac.th>
2020-02-12 21:10:31 +0700

    HW5

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -1,10 +1,191 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyAHQVNcB3dMtQ4B9M-WcTundSA4ogkAbnU",
-    authDomain: "homework5-8e7a6.firebaseapp.com",
-    projectId: "homework5-8e7a6",
+    apiKey: "AIzaSyBjwyWgB7aYgwQQ6Z4XFoRBMwjmazKRW2M",
+    authDomain: "lab5-firebase-7ff1d.firebaseapp.com",
+    projectId: "lab5-firebase-7ff1d"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
+let i = 0
+let countMale = 0;
+let countFemale = 0;
+let countOther = 0;
+let id = []
 
-let subform = document.querySelector('.reset')
+$('#reset').click(() => reset())
+
+$('#save').click((dbUser) => {
+     if (!validateText('name')) $('#name').focus()
+    else if (!validateEmail('email')) $('#email').focus()
+    else if (!validateText('message')) $('#message').focus()
+    else {
+        db.collection("contact").add({
+            name: $('#name').val(),
+            gender: getGender(),
+            email: $('#email').val(),
+            message: $('#message').val(),
+        }).then(function (docRef) {
+            reset()
+            console.log("Document written with ID: ", docRef.id)
+            alert("Message saved !")
+
+        }).catch(function (error) {
+            console.error("Error adding document: ", error)
+            alert("Error adding document: ", error)
+        })
+    }
+})
+
+function reset() {
+    // $('#subject').val('')
+    // removeValidate('subject')
+    $('#name').val('')
+    removeValidate('name')
+    // $('#phone').val('')
+    // removeValidate('phone')
+    $('#gender').val('1')
+    $('#name').focus()
+    $('#email').val('')
+    removeValidate('email')
+    $('#message').val('')
+    removeValidate('message')
+
+}
+
+function getGender() {
+    let radios = document.getElementsByName('gender');
+    for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            return i + 1;
+        }
+    }
+}
+
+function validateText(id) {
+    if ($("#" + id).val() == null || $("#" + id).val() == "") {
+        validateShow(id, false)
+        return false;
+    }
+    else {
+        validateShow(id, true)
+        return true;
+    }
+
+}
+
+function validateEmail(id) {
+    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if (!email_regex.test($("#" + id).val())) {
+        validateShow(id, false)
+        return false;
+    }
+    else {
+        validateShow(id, true)
+        return true;
+    }
+}
+
+// function validatePhone(id) {
+//     var phoneno = /^\d{10}$/;
+//     if (!$("#" + id).val().match(phoneno)) {
+//         validateShow(id, false)
+//         return false;
+//     }
+//     else {
+//         validateShow(id, true)
+//         return true;
+//     }
+// }
+
+function validateShow(id, fact) {
+    if (fact) {
+        var div = $("#" + id).closest("div");
+        div.removeClass("has-error");
+        $("#glypcn" + id).remove();
+        div.addClass("has-success has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+    } else {
+        $("#" + id).val('')
+        let div = $("#" + id).closest("div");
+        div.removeClass("has-success");
+        $("#glypcn" + id).remove();
+        div.addClass("has-error has-feedback");
+        div.append('<span id="glypcn' + id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+
+    }
+}
+
+function removeValidate(id) {
+    var div = $("#" + id).closest("div");
+    div.removeClass("has-error");
+    div.removeClass("has-success");
+    $("#glypcn" + id).remove();
+}
+
+db.collection('contact').orderBy('name').onSnapshot(doc => {
+    let table = $('tbody')[0]
+    $('tbody tr').remove()
+    i = 0
+    countMale = 0;
+    countFemale = 0;
+    countOther = 0;
+    id = []
+    doc.forEach(item => {
+        id[i] = item.id
+        i++
+        let row = table.insertRow(-1)
+        let firstCell = row.insertCell(0)
+        let secondCell = row.insertCell(1)
+        let thirdCell = row.insertCell(2)
+        let email = item.data().email
+        let emailHide = ''
+        firstCell.textContent = item.data().name
+        switch (Number(item.data().gender)) {
+            case 1:
+                secondCell.textContent = 'Male'
+                countMale++
+                break
+            case 2:
+                secondCell.textContent = 'Female'
+                countFemale++
+                break
+            default:
+                secondCell.textContent = 'Other'
+                countOther++
+        }
+        for (let i = 0; i < email.length; i++) {
+            if (email[i] != '@' && email[i] != '.') emailHide += 'x'
+            else emailHide += email[i]
+
+        }
+        thirdCell.textContent = emailHide
+        // fourthCell.innerHTML = "<button id='more' type='button' class='btn btn-primary btn-xs' data-toggle='modal' data-target='#detailModel'>more</button>"
+        // fourthCell.innerHTML += " "
+        // fourthCell.innerHTML += "<button id='delete' type='button' class='btn btn-danger btn-xs'>delete</button>"
+        // $('#titleModel').text(item.data().name)
+
+    })
+    if (i == 0) {
+        let row = table.insertRow(-1)
+        let firstCell = row.insertCell(0)
+        let secondCell = row.insertCell(1)
+        let thirdCell = row.insertCell(2)
+        firstCell.textContent = 'Empty'
+        secondCell.textContent = 'Empty'
+        thirdCell.textContent = 'Empty'
+        document.getElementById("gen1").style.width = "0%"
+        document.getElementById("gen2").style.width = "0%"
+        document.getElementById("gen3").style.width = "0%"
+        document.getElementById("t1").innerHTML = "0%"
+        document.getElementById("t2").innerHTML = "0%"
+        document.getElementById("t3").innerHTML = "0%"
+    } else {
+        document.getElementById("gen1").style.width = 100 * (countMale / i) + "%";
+        document.getElementById("gen2").style.width = 100 * (countFemale / i) + "%";
+        document.getElementById("gen3").style.width = 100 * (countOther / i) + "%";
+        document.getElementById("t1").innerHTML = 100 * (countMale / i) + "%";
+        document.getElementById("t2").innerHTML = 100 * (countFemale / i) + "%";
+        document.getElementById("t3").innerHTML = 100 * (countOther / i) + "%";
+    }
+})
